87d8af4fa293490face25e80088f7e6a1c07e565
TIAN XIA <summer@TIANs-MacBook-Pro.local>
2022-05-19 23:30:22 +0200

    v2

---
+++ mysite/polls/models.py
@@ -0,0 +1,28 @@
+import datetime
+from django.contrib import admin
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('date ended')
+    def __str__(self):
+        return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Vote ended?',
+    )
+    def was_ended(self):
+        return self.end_date <= timezone.now()
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
---
+++ post_no.php
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<vxml version = "2.1" application="http://webhosting.voxeo.net/208143/www/root.vxml">
+
+<?php
+/**
+ * 发送post请求
+ * @param string $url 请求地址
+ * @param array $post_data post键值对数据
+ * @return string
+ */
+function send_post($url, $post_data) {
+ 
+    $postdata = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata,
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+ 
+    return $result;
+}
+
+$post_data = array(
+    'choice' => '4'
+);
+send_post('http://13.38.249.239:8000/polls/3/vote/', $post_data);
+echo "DONE"
+?>
+
+</vxml>
