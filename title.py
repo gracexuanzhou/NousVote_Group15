0eb00dbc5b0b8090d88f30dc322c67700c42630b
TIAN XIA <summer@TIANs-MacBook-Pro.local>
2022-05-22 23:53:46 +0200

    v3.0

--- title.py
+++ title.py
@@ -1,5 +1,11 @@
 from requests_html import HTMLSession
-url = "http://13.38.249.239:8000/polls/3/results/"
+from subprocess import check_output
+import json
+
+config = check_output(['php', '-r', 'echo json_encode(include "config.php");'])
+config = json.loads(config)
+
+url = config['RESULT']
 session = HTMLSession()
 r = session.get(url)
 t = r.html.text.split("\n")[0]
